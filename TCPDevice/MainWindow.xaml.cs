e488ee20871e2d6839a8bfc384252bdf3ed5397e
signa <fuper.puper@gmail.com>
2024-03-21 09:31:34 +0700

    Made server chat window bigger Changed its location Made commands look better

--- TCPDevice/MainWindow.xaml.cs
+++ TCPDevice/MainWindow.xaml.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net.Sockets;
-using System.Configuration;
 using System.Net;
-using System.Windows.Markup;
 using System.IO;
 using System.Text.RegularExpressions;
+
 #pragma warning disable CS8618
 #pragma warning disable CS8602
 
