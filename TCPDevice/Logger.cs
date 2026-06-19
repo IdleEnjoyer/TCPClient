c0982c5d5392e857d664e9f55a4d88c9b0f2caca
Артемий <soimetra@yandex.ru>
2026-06-19 09:06:04 +0700

    Added Logging, Started Attestations

---
+++ TCPDevice/Logger.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Path = System.IO.Path;
+
+namespace TCPDevice
+{
+	internal class Logger : IDisposable
+	{
+		public enum LogType
+		{
+			SEND_LOG,
+			GET_LOG,
+			ERR_LOG,
+			INFO_LOG
+		}
+
+		private StreamWriter FileStream;
+
+		string DefaultLogPath = AppDomain.CurrentDomain.BaseDirectory + $"\\Logs\\{DateTime.Now.ToString().Replace(':','_')} Log.txt";
+
+		public Logger()
+		{
+			FileStream = File.CreateText(DefaultLogPath);
+			FileStream.WriteLine($"{DateTime.Now}: ИНФО     : НАЧАЛО ЛОГИРОВАНИЯ");
+		}
+
+		public Logger(string FilePath)
+		{
+			FileStream = File.CreateText(FilePath + $"\\{DateTime.Now.ToString().Replace(':', '_')} Log.txt");
+			FileStream.WriteLine($"{DateTime.Now}: ИНФО     : НАЧАЛО ЛОГИРОВАНИЯ");
+		}
+
+		public void Dispose()
+		{
+			FileStream.WriteLine($"{DateTime.Now}: ИНФО     : КОНЕЦ ЛОГИРОВАНИЯ");
+			FileStream.Close();
+		}
+
+		public bool WriteLog(LogType Type, string Message)
+		{
+			switch (Type)
+			{
+				case LogType.SEND_LOG:
+					try
+					{
+						FileStream.WriteLine($"{DateTime.Now}: ОТПРАВКА : {Message}");
+						return true;
+					}
+					catch
+					{
+						return false;
+					}
+				case LogType.GET_LOG:
+					try
+					{
+						FileStream.WriteLine($"{DateTime.Now}: ПОЛУЧЕНИЕ: {Message}");
+						return true;
+					}
+					catch
+					{
+						return false;
+					}
+				case LogType.ERR_LOG:
+					try
+					{
+						FileStream.WriteLine($"{DateTime.Now}: ОШИБКА   : {Message}");
+						return true;
+					}
+					catch
+					{
+						return false;
+					}
+				case LogType.INFO_LOG:
+					try
+					{
+						FileStream.WriteLine($"{DateTime.Now}: ИНФО     : {Message}");
+						return true;
+					}
+					catch
+					{
+						return false;
+					}
+				default:
+					return false;
+			}
+		}
+	}
+}
--- TCPDevice/MainWindow.xaml.cs
+++ TCPDevice/MainWindow.xaml.cs
@@ -25,6 +25,10 @@ using System.Text.RegularExpressions;
 using System.Drawing;
 using Color = System.Windows.Media.Color;
 using Point = System.Windows.Point;
+using Microsoft.Win32;
+using WindowsAPICodePack.Dialogs;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace TCPDevice
 {
@@ -33,46 +37,60 @@ namespace TCPDevice
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		Properties.Settings AppSettings = new Properties.Settings();
+
+		private const int ABS_VALID  = 0b_000000000111;
+		private const int INC_VALID  = 0b_000000111000;
+		private const int STEP_VALID = 0b_001111000000;
+		private const int VEL_VALID  = 0b_110000000000;
+
 		private TcpClient Client_OPU1;
 		private NetworkStream Stream_OPU1;
-		private double OPU1_LowerLimits;
-		private double OPU1_UpperLimits;
-		private double OPU1_LowerVelLimits;
-		private double OPU1_UpperVelLimits;
-		private bool OPU1_Enabled = false;
 		private double OPU1_Position;
-		private bool OPU1_Stopped = false;
-		private bool OPU1_InError = false;
+		private double OPU1_Velocity;
 
 		private DispatcherTimer OPU1_StatusTimer = new DispatcherTimer();
 		private DispatcherTimer OPU_PosTimer = new DispatcherTimer();
+		private DispatcherTimer StepTimer = new DispatcherTimer();
 		private int OPU1_Status = 0;
 		private byte[] WriteByteData;
-		public string Axis_OPU1 = "";
-		public bool AllowNegative = false;
-
-		private int OPU1_IngoreLimit_Counter = 0;
-		private Regex ValidateRealNumber = new Regex(@"^-?[0-9]+(\\.[0-9]+)?$");
+		private int ValidationFlags = 0b_111111111111;
+		private bool IsStepping = false;
 
 		private RadialGradientBrush GreenBrush = new(Color.FromRgb(255, 255, 255), Color.FromRgb(0, 255, 0));
 		private RadialGradientBrush RedBrush = new(Color.FromRgb(255, 255, 255), Color.FromRgb(255, 0, 0));
 		private NumberFormatInfo DoubleFormat = new NumberFormatInfo();
+
+		private Logger DataLog = new Logger();
 		public MainWindow()
         {
             InitializeComponent();
 			GreenBrush.Center = new Point(0.25, 0.25);
 			RedBrush.Center = new Point(0.25, 0.25);
 			DoubleFormat.NumberDecimalSeparator = ".";
+
 			OPU1_StatusTimer.Interval = TimeSpan.FromMilliseconds(100);
 			OPU1_StatusTimer.Tick += OPU1_StatusTimer_Tick;
+
 			OPU_PosTimer.Interval = TimeSpan.FromMilliseconds(50);
 			OPU_PosTimer.Tick += OPU_PosTimer_Tick;
+
+			StepTimer.Tick += StepTimer_Tick;
+		}
+
+		private void StepTimer_Tick(object? sender, EventArgs e)
+		{
+			if (Client_OPU1 != null)
+			{
+				SendCommand($"MOVER {Step_TgtStep} {Step_TgtVel} {Step_TgtAcc}");
+				StepTimer.Stop();
+			}
 		}
 
 		private void OPU_PosTimer_Tick(object? sender, EventArgs e)
 		{
 			if (Client_OPU1 != null) {
-				SendCommand_OPU1("POS?");
+				SendCommand("POS?");
 			}
 		}
 
@@ -83,64 +101,73 @@ namespace TCPDevice
 				switch (OPU1_Status)
 				{
 					case 0:
-						SendCommand_OPU1("EN?");
+						SendCommand("EN?");
 						OPU1_Status++;
 						break;
 					case 1:
-						SendCommand_OPU1("FH?");
+						SendCommand("FH?");
 						OPU1_Status++;
 						break;
 					case 2:
-						SendCommand_OPU1("INIT?");
+						SendCommand("INIT?");
 						OPU1_Status++;
 						break;
 					case 3:
-						SendCommand_OPU1("STOP?");
+						SendCommand("STOP?");
 						OPU1_Status++;
 						break;
 					case 4:
-						SendCommand_OPU1("FLT?");
+						SendCommand("FLT?");
 						OPU1_Status = 0;
 						break;
 				}
 			}
 		}
 
-		public void SendCommand_OPU1(string Com)
+		public void SendCommand(string Com)
 		{
 			if (Client_OPU1.Connected)
 			{
 				WriteByteData = Encoding.UTF8.GetBytes("^" + Com.Replace(',','.') + "~\r\n");
 				Stream_OPU1.Write(WriteByteData, 0, WriteByteData.Length);
+				DataLog.WriteLog(Logger.LogType.SEND_LOG, "^" + Com.Replace(',', '.') + "~\r\n");
 			}
 		}
 
-		private async void ConnectOPU1()
+		private async void ConnectOPU()
 		{
 			try
 			{
 				IPAddress Address = IPAddress.Parse("192.168.0.101");
 				int Port = 2000;
 				Client_OPU1 = new TcpClient(Address.ToString(), Port);
+				DataLog.WriteLog(Logger.LogType.INFO_LOG, "Подключение");
 
 				if (Client_OPU1.Connected)
 				{
 					//ConnectStatus.Fill = GreenBrush;
 					Stream_OPU1 = Client_OPU1.GetStream();
+					ConnectionCheck.IsChecked = true;
+					OPU_PosTimer.Start();
 					OPU1_StatusTimer.Start();
-					await StartReadingOPU1DataAsync();
+					DataLog.WriteLog(Logger.LogType.INFO_LOG, "Подключено!");
+					await StartReadingDataAsync();
 				}
 				else
 				{
 					//ConnectStatus.Fill = RedBrush;
+					ConnectionCheck.IsChecked = false;
+					DataLog.WriteLog(Logger.LogType.ERR_LOG, "Не удалось подключиться");
 				}
 			}
 			catch(Exception ex) { 
 				MessageBox.Show(ex.Message);
+				DataLog.WriteLog(Logger.LogType.ERR_LOG, "Не удалось подключиться");
+				ConnectionCheck.IsChecked = false;
 			}
 		}
 
-		private async Task StartReadingOPU1DataAsync()
+		private async Task StartReadingDataAsync()
 		{
 			try
 			{
@@ -156,42 +183,62 @@ namespace TCPDevice
 							break;
 						}
 						string Data = Encoding.UTF8.GetString(Buffer, 0, BytesRead);
-
+						DataLog.WriteLog(Logger.LogType.GET_LOG, Data);
 						switch (Data)
 						{
 							case "^EN?:1~":
 								PowerCheck.IsChecked = true;
 								break;
 							case "^STOP?:1~":
-								
+								MovingCheck.IsChecked = true;
+								MoveStatus.Fill = Brushes.Green;
+								if (IsStepping && StepTimer.IsEnabled)
+								{
+									StepTimer.Stop();
+								}
 								break;
 							case "^FLT?:0~":
 								InFaultCheck.IsChecked = false;
+								break;
+							case "^INIT?:1~":
+								InitCheck.IsChecked = true;
 								break;
 							default:
 								if (Data.Contains("EN?"))
 								{
 									PowerCheck.IsChecked = false;
+									IsStepping = false;
 								}
 								if (Data.Contains("STOP?"))
 								{
-									OPU1_Stopped = false;
+									MovingCheck.IsChecked = false;
 									MoveStatus.Fill = Brushes.Red;
-									
+									if (IsStepping)
+									{
+										StepTimer.Start();
+									}
 								}
 								if (Data.Contains("FLT?"))
 								{
 									InFaultCheck.IsChecked = true;
+									IsStepping = false;
+								}
+								if (Data.Contains("INIT?"))
+								{
+									InitCheck.IsChecked = false;
+									IsStepping = false;
 								}
 								if (Data.Contains("POS?"))
 								{
-									string[] Positions = Data.Substring(Data.IndexOf(":") + 1).Split(":");
-									int Index = 0;
-									foreach (string Position in Positions)
-									{
-										OPU1_Position = double.Parse(Position.Replace('.',',').Replace("~",string.Empty));
-										Index++;
-									}
+									string Position = Data.Substring(Data.IndexOf(":") + 1).Replace("~", string.Empty);
+									OPU1_Position = double.Parse(Position, DoubleFormat);
+									Info_CurPos.Text = OPU1_Position.ToString();
+								}
+								if (Data.Contains("VEL?"))
+								{
+									string Velocity = Data.Substring(Data.IndexOf(":") + 1).Replace("~", string.Empty);
+									OPU1_Velocity = double.Parse(Velocity, DoubleFormat);
+									Info_CurVel.Text = OPU1_Velocity.ToString();
 								}
 								break;
 						}
@@ -199,7 +246,10 @@ namespace TCPDevice
 					catch (IOException)
 					{
 						MessageBox.Show("Connection error");
+						DataLog.WriteLog(Logger.LogType.ERR_LOG, "Ошибка подключения при чтении");
 						//ConnectStatus.Fill = RedBrush;
+						ConnectionCheck.IsChecked = false;
+						OPU_PosTimer.Stop();
 						OPU1_StatusTimer.Stop();
 						break;
 					}
@@ -208,14 +258,12 @@ namespace TCPDevice
 			catch (Exception ex)
 			{
 				MessageBox.Show($"Connection Error: {ex.Message}");
+				DataLog.WriteLog(Logger.LogType.ERR_LOG, "Ошибка подключения при чтении");
 				//ConnectStatus.Fill = RedBrush;
+				ConnectionCheck.IsChecked = false;
+				OPU_PosTimer.Stop();
 				OPU1_StatusTimer.Stop();
 			}
-		}
-
-		private void OPU1_Connect_Click(object sender, RoutedEventArgs e)
-		{
-			ConnectOPU1();
 		}
 
 		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -224,32 +272,10 @@ namespace TCPDevice
 			{
 				if (Client_OPU1.Connected)
 				{
-					SendCommand_OPU1("DIS");
+					SendCommand("DIS");
 				}
 			}
-		}
-
-		private void OPU1_Power_Click(object sender, RoutedEventArgs e)
-		{
-			if (!OPU1_Enabled)
-			{
-				SendCommand_OPU1("EN");
-				SendCommand_OPU1("FH");
-			}
-			else
-			{
-				SendCommand_OPU1("DIS");
-			}
-		}
-
-		private void OPU1_Stop_Click(object sender, RoutedEventArgs e)
-		{
-			SendCommand_OPU1("STOP");
-		}
-
-		private void OPU1_Clear_Click(object sender, RoutedEventArgs e)
-		{
-			SendCommand_OPU1("CLR");
+			DataLog.Dispose();
 		}
 
 		private void LeftPanelSize_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
@@ -275,45 +301,120 @@ namespace TCPDevice
 
 		private void Power_Click(object sender, RoutedEventArgs e)
 		{
-			PowerCheck.IsChecked = !PowerCheck.IsChecked;
+			if (Client_OPU1 != null)
+			{
+				IsStepping = false;
+				if (PowerCheck.IsChecked == false)
+				{
+					if (MessageBox.Show("ВНИМАНИЕ!\n\nПосле подачи питания начнётся плавное движение в позицию нуля!\n\nПродолжить?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+					{
+						SendCommand("EN");
+					}
+				}
+				else
+				{
+					SendCommand("DIS");
+				}
+			}
+			
+			
 		}
 
 		private void Connection_Click(object sender, RoutedEventArgs e)
 		{
-			ConnectionCheck.IsChecked = !ConnectionCheck.IsChecked;
+			if (Client_OPU1 == null)
+			{
+				ConnectOPU();
+			}
+			else
+			{
+				IsStepping = false;
+				Client_OPU1.Close();
+				DataLog.WriteLog(Logger.LogType.INFO_LOG, "Отключение пользователем от сервера");
+			}
 		}
 
 		private void Fault_Click(object sender, RoutedEventArgs e)
 		{
-			InFaultCheck.IsChecked = !InFaultCheck.IsChecked;
+			if (Client_OPU1 != null)
+			{
+				SendCommand("CLR");
+			}
 		}
 
 		private void Abs_StartMove_Click(object sender, RoutedEventArgs e)
 		{
 			if (Client_OPU1 != null)
 			{
-				if (PowerCheck.IsChecked == false)
+				IsStepping = false;
+				if (PowerCheck.IsChecked == true)
 				{
-					MessageBox.Show("Нет Питания!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+					SendCommand($"MOVEA {Abs_TgtPos} {Abs_TgtVel} {Abs_TgtAcc}");
 				}
 				else
 				{
-					if (Abs_TgtPos.Text != string.Empty && Abs_TgtVel.Text != string.Empty && Abs_TgtAcc.Text != string.Empty)
-					{
-						
-					}
+					MessageBox.Show("Нет Питания!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
 				}
 			}
-			else
-			{
-				MessageBox.Show("Нет Подключения!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-			}
-			
         }
 
-		private void Abs_StopMove_Click(object sender, RoutedEventArgs e)
+		private void Inc_StartMove_Click(object sender, RoutedEventArgs e)
 		{
+			if (Client_OPU1 != null)
+			{
+				IsStepping = false;
+				if (PowerCheck.IsChecked == true)
+				{
+					SendCommand($"MOVER {Inc_TgtPos} {Inc_TgtVel} {Inc_TgtAcc}");
+				}
+				else
+				{
+					MessageBox.Show("Нет Питания!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+				}
+			}
+		}
 
+		private void Step_StartMove_Click(object sender, RoutedEventArgs e)
+		{
+			if (Client_OPU1 == null)
+			{
+				if (PowerCheck.IsChecked == true)
+				{
+					StepTimer.Interval = TimeSpan.FromMilliseconds(double.Parse(Step_TgtPause.Text) * 1000.0);
+					SendCommand($"MOVER {Step_TgtStep} {Step_TgtVel} {Step_TgtAcc}");
+					IsStepping = true;
+				}
+				else
+				{
+					MessageBox.Show("Нет Питания!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+				}
+			}
+			
+		}
+
+		private void Vel_StartMove_Click(object sender, RoutedEventArgs e)
+		{
+			if (Client_OPU1 != null)
+			{
+				IsStepping = false;
+				if (PowerCheck.IsChecked == true)
+				{
+					SendCommand($"MOVEV {Vel_TgtVel} {Vel_TgtAcc}");
+				}
+				else
+				{
+					MessageBox.Show("Нет Питания!", "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+				}
+			}
+		}
+
+		private void StopMove_Click(object sender, RoutedEventArgs e)
+		{
+			if (Client_OPU1 != null)
+			{
+				SendCommand("STOP");
+				IsStepping = false;
+			}
 		}
 
 		private void NumberInputTextbox_LostFocus(object sender, RoutedEventArgs e)
@@ -322,11 +423,76 @@ namespace TCPDevice
 			if (!double.TryParse(((TextBox)sender).Text, DoubleFormat, out Num))
 			{
 				((TextBox)sender).BorderBrush = Brushes.Red;
+				ValidationFlags |= int.Parse((string)(((TextBox)sender).Tag));
 			}
 			else
 			{
 				((TextBox)sender).BorderBrush = new SolidColorBrush(Color.FromArgb(0xff, 0xAB, 0xAd, 0xB3));
+				ValidationFlags &= (ABS_VALID | INC_VALID | STEP_VALID | VEL_VALID) - int.Parse((string)((TextBox)sender).Tag);
 			}
+            if ((ValidationFlags & ABS_VALID) != 0)
+            {
+				Abs_StartMove.IsEnabled = false;
+            }
+			if ((ValidationFlags & INC_VALID) != 0)
+			{
+				Inc_StartMove.IsEnabled = false;
+			}
+			if ((ValidationFlags & STEP_VALID) != 0)
+			{
+				Step_StartMove.IsEnabled = false;
+			}
+			if ((ValidationFlags & VEL_VALID) != 0)
+			{
+				Vel_StartMove.IsEnabled = false;
+			}
+		}
+
+		private void TreeViewItem_RequestBringIntoView(object sender, RequestBringIntoViewEventArgs e)
+		{
+			e.Handled = true;
+		}
+
+		private void Init_Click(object sender, RoutedEventArgs e)
+		{
+			if (Client_OPU1 != null)
+			{
+				if (MessageBox.Show("При инииализации будет совершено быстрое резкое движение!\n\nПродолжить?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
+				{
+					SendCommand("INIT");
+				}
+			}
+		}
+
+		private void Attest_Variant_DigMeasSystErr_Start_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void MenuLogging_Click(object sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void LoggingFolderChange_Click(object sender, RoutedEventArgs e)
+		{
+			CommonOpenFileDialog COFD = new CommonOpenFileDialog();
+			COFD.Multiselect = false;
+			COFD.IsFolderPicker = true;
+			if (COFD.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				AppSettings.LogPath = COFD.FileName;
+				AppSettings.Save();
+				DataLog.WriteLog(Logger.LogType.INFO_LOG, $"Изменение папки хранения логов: {COFD.FileName}");
+				DataLog.Dispose();
+
+				DataLog = new Logger(AppSettings.LogPath);
+			}
+			
 		}
 	}
 }
+//TODO:
+//EN - предупреждение о движении в ноль
+//INIT - предупредение о резком старте движения
+//SH - предупреждение о снятии и подаче питания на позиционер
